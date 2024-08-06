2409bfd0094a560809af42695a0fcf70b5504535
mahmoodhamdi <hmdy7486@gmail.com>
2024-08-06 08:34:50 +0300

    Add Custom Dropdown Button

--- lib/features/anime/presentation/views/anime_view.dart
+++ lib/features/anime/presentation/views/anime_view.dart
@@ -1,3 +1,6 @@
+import 'package:anime_app/core/constants/app_colors.dart';
+import 'package:anime_app/core/constants/app_text_styles.dart';
+import 'package:anime_app/core/widgets/app_dropdown_button_form_field.dart';
 import 'package:anime_app/core/widgets/app_scaffold.dart';
 import 'package:flutter/material.dart';
 
@@ -6,6 +9,32 @@ class AnimeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const AppScaffold();
+    return AppScaffold(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 64,
+            horizontal: 120,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Anime',
+                style: AppTextStyles.heading1.copyWith(color: AppColors.grey50),
+              ),
+              const SizedBox(height: 24),
+              SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.2,
+                  child: const AppDropdownButtonFormField()),
+              const SizedBox(height: 24),
+              Text("120 items",
+                  style: AppTextStyles.bodyRegular
+                      .copyWith(color: AppColors.grey400)),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
